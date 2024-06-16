ecc352500ade0cd6f38e68e0c8199ed5fa29aaa3
deshan-salitha <deshan.salitha@outlook.com>
2024-06-17 02:01:20 +0530

    deployment

---
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PlaceOrder.css'
+
+function PlaceOrder(props) {
+    return (
+        <div>
+            Place order
+        </div>
+    );
+}
+
+export default PlaceOrder;
